8d204980182a9457eb2d5a1ff9d534b6cfb07cfc
Tyson Gach <tyson@tysongach.com>
2017-03-18 19:50:16 -0400

    Clean up
    - Remove comments from gitignore template
    - Remove comments from Gemfile
    - Clean up comments from config.rb
    - Clean up layout

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Default Middleman Project Template
+
+This is the [Middleman] template used when you run `middleman init`.
+
+[Middleman]: https://middlemanapp.com/
--- template/config.rb
+++ template/config.rb
@@ -1,39 +1,39 @@
-###
-# Page options, layouts, aliases and proxies
-###
+# Layouts
+# https://middlemanapp.com/basics/layouts/
 
-# Per-page layout changes:
-#
-# With no layout
+# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page '/path/to/file.html', layout: 'other_layout'
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
 
-# General configuration
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
 
-###
 # Helpers
-###
-
 # Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
 # Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-end
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
